2d1871da6b9d28708f4a27ea2abb01b17af3c61f
Deivid Marreiro <davidteixeiro@gmail.com>
2023-01-31 21:24:24 -0300

    Remodeling Show Model

--- app.py
+++ app.py
@@ -72,7 +72,6 @@ class Show(db.Model):
     __tablename__ = "shows"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey("venues.id"), nullable=False)
